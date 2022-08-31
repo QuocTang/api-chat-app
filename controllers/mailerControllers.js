b7a84d1c25e4c9a5e7a744ac86817445bb281520
QuocTang <76061383+QuocTang@users.noreply.github.com>
2022-08-31 14:29:23 +0700

    Update mailerControllers.js

--- controllers/mailerControllers.js
+++ controllers/mailerControllers.js
@@ -10,14 +10,24 @@ const transporter = nodeMailer.createTransport({
 });
 
 const sendMailAuthencation = asyncHandler(async (req, res) => {
-  const { email } = req.body;
+  const { name, email } = req.body;
   let randomCapcha = (Math.random() + 1).toString(36).substring(7);
   console.log(email);
   const options = {
     from: "abclone114@gmail.com",
     to: email,
-    subject: "[ChatApp] Please verify your email!",
-    text: randomCapcha,
+    subject: "[ChatApp] Please verify your email",
+    html: `
+<h2>Hey ${name}!</h2>
+    
+<h5>A sign in attempt requires further verification because we did not reconize your device.
+ To complete the sign in, enter the verification code on the unrecognized device.</h5>
+
+<h4>Verification code: <span style="color:red">${randomCapcha}</span></h4>
+
+<h5>Thanks</h5>
+
+`,
   };
   await transporter.sendMail(options, (err, info) => {
     if (err) {
