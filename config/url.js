ac04db69763356c8f86f050d55a04125c53d440d
QuocTang <76061383+QuocTang@users.noreply.github.com>
2022-08-31 10:05:29 +0700

    Update url.js

--- config/url.js
+++ config/url.js
@@ -1,3 +1,10 @@
-const URL = "http://localhost:3000";
+let URL = "";
+let environment = "proc"
+
+if(environment === "proc"){
+  URL = "https://socket-io-mernstack-chat-app.netlify.app";
+}else if(environment === "local"){
+  URL = "http://localhost:3000";
+}
 
 module.exports = { URL };
